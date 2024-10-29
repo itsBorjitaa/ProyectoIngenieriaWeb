09f18565bfb022ee78fedd8ea80ffc79638ce251
Borja Rabade <145967062+itsBorjitaa@users.noreply.github.com>
2024-10-29 13:28:32 +0100

    Creación de proyecto

---
+++ proyectoIngenieriaWeb/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include  # Asegúrate de importar 'include'
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('appPeliculas/', include('appPeliculas.urls')),
+]
